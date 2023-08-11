35b814d9ff99c6fb542f3cfd80dbe7e0f94ff612
hayone1 <boluwatife.morawo@stu.cu.edu.ng>
2023-08-11 11:44:19 +0100

    working

--- js-config.js
+++
@@ -1,5 +0,0 @@
-var config = {
-	google_client_id: "525178101879-vo4g2utk6asu84g2l6idkc2u6oaf74uj.apps.googleusercontent.com",
-	google_auth_callback: "/api/v1/identity/LoginCallback",
-	auth_redirect: "/authentication/login-callback"
-};
